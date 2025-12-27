b6b3d5a36c0725ef0b808367669b8cdaff937efe
vishka_0308 <akavish0310@gmail.com>
2025-12-27 12:51:36 +0530

    assigment-1

---
+++ assigment-1/program-1.py
@@ -0,0 +1,7 @@
+a=int(input("Enter first number : "))
+b=int(input("Enter second number : "))
+print("the sum is :",a+b)
+print("the diff is :",a-b)
+print("the product is :",a*b)
+print("the ratio is :",a/b)
+
---
+++ assigment-1/program2.py
@@ -0,0 +1,4 @@
+f_name=input("Enetr your first name : ")
+l_name=input("Enetr your last name : ")
+full_name=f_name+l_name
+print(f"Welcome {full_name} , this is my python program")
